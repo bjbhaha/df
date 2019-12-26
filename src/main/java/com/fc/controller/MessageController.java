7b2621caf690eea35e26c8df5345694c6a530381
pyt(ToTkr2) <58772909+totkr767907026@users.noreply.github.com>
2019-12-26 10:34:22 +0800

    Update MessageController.java

--- src/main/java/com/fc/controller/MessageController.java
+++ src/main/java/com/fc/controller/MessageController.java
@@ -1,6 +1,10 @@
 package com.fc.controller;
 
-
+import com.fc.model.Message;
+import com.fc.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,14 +20,5 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
-    //去消息页面
-    @RequestMapping("/toMessage.do")
-    public String toMessage(Model model, HttpSession session) {
-        Integer sessionUid = (Integer) session.getAttribute("uid");
-        Map<String,List<Message>> map = messageService.listMessageByUid(sessionUid);
-        model.addAttribute("map",map);
-        System.out.println(map);
-        return "message";
-    }
 
 }
