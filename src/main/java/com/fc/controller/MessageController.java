9b79a07eb2e99ec7a0c127871008a672237a7dc8
pyt(ToTkr2) <58772909+totkr767907026@users.noreply.github.com>
2019-12-26 10:34:31 +0800

    Update MessageController.java

--- src/main/java/com/fc/controller/MessageController.java
+++ src/main/java/com/fc/controller/MessageController.java
@@ -20,5 +20,15 @@ public class MessageController {
     @Autowired
     private MessageService messageService;
 
+    
+    //去消息页面
+    @RequestMapping("/toMessage.do")
+    public String toMessage(Model model, HttpSession session) {
+        Integer sessionUid = (Integer) session.getAttribute("uid");
+        Map<String,List<Message>> map = messageService.listMessageByUid(sessionUid);
+        model.addAttribute("map",map);
+        System.out.println(map);
+        return "message";
+    }
 
 }
