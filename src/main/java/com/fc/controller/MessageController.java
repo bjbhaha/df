6c701ac9d9f903c6b8b21642fce1572840097fcd
pyt(ToTkr2) <58772909+totkr767907026@users.noreply.github.com>
2019-12-26 10:34:11 +0800

    Update MessageController.java

--- src/main/java/com/fc/controller/MessageController.java
+++ src/main/java/com/fc/controller/MessageController.java
@@ -1,11 +1,7 @@
 package com.fc.controller;
 
 
-import com.fc.model.Message;
-import com.fc.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
