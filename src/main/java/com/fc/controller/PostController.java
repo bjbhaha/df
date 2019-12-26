c7465f2bf114ae6b89cfddfdb148e3ffabb10942
pyt(ToTkr2) <58772909+totkr767907026@users.noreply.github.com>
2019-12-26 10:35:04 +0800

    Update PostController.java

--- src/main/java/com/fc/controller/PostController.java
+++ src/main/java/com/fc/controller/PostController.java
@@ -105,4 +105,18 @@ public class PostController {
         PrintWriter out = response.getWriter();
         out.write(jsonStr);
     }
+    
+        //去发帖的页面2
+    @RequestMapping("/toPublish2.do")
+    public String toPublish2(Model model){
+        List<Topic> topicList = topicService.listTopic();
+        model.addAttribute("topicList",topicList);
+        return "publish2";
+    }
+    //发帖2
+    @RequestMapping("/publishPost2.do")
+    public String publishPost2(Post post) {
+        int id = postService.publishPost(post);
+        return "redirect:toPost.do?pid="+id;
+    }
 }
